e7f655f8e5a3f887501e8d411f01f9ead22c274a
JoaoLimaHeyla <joao.lima@sptech.school>
2023-03-09 22:26:44 -0300

    Prototipo2

--- prototipo/continuacao.js
+++ prototipo/continuacao.js
@@ -1,25 +1,29 @@
-const proximo = document.getElementById('oi');
+const proximo = document.getElementById('butao');
+const caixadados2 = document.getElementById('formulario');
+const caixadados = document.getElementById('formulario-back');
 const seta = document.getElementById('arrowLeft');
-const caixadados2 = document.getElementById('caixadados2');
 
 proximo.addEventListener('click', () => {
     
     setTimeout(() => {
         caixadados2.style.opacity = 1;
+        caixadados.style.opacity = 0;
         arrowLeft.style.opacity = 1;            
-    }, 200);
+    }, 500);
     caixadados2.style.display = 'flex';
+    caixadados.style.display = 'none';
     arrowLeft.style.display = 'flex';
-
 })
-7
+
 seta.addEventListener('click', () => {
     
     setTimeout(() => {
-        caixadados2.style.opacity = 0;            
+        caixadados2.style.opacity = 0;
+        caixadados.style.opacity = 1;           
         seta.style.opacity = 0;
         setTimeout(() => {
-            caixadados2.style.display = 'none'
+            caixadados.style.display = 'flex';
+            caixadados2.style.display = 'none';
             seta.style.display = 'none';
         }, 400);
     }, 400);
