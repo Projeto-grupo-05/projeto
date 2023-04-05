a0897504e54b15e5c21e96305b3901e8e9616e4f
emilliedev <emillie.souza@sptech.school>
2023-04-05 00:49:49 -0300

    fix/reaplicando mudanças antes da refatoração

--- site/src/controllers/usuarioController.js
+++ site/src/controllers/usuarioController.js
@@ -65,18 +65,16 @@ function cadastrar(req, res) {
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
-
+    var token = req.body.tokenServer;
     // Faça as validações dos valores
-    if (nome == undefined) {
-        res.status(400).send("Seu nome está undefined!");
-    } else if (email == undefined) {
-        res.status(400).send("Seu email está undefined!");
+    if (token == undefined) {
+        res.status(400).send("Seu token está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
     } else {
         
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha)
+        usuarioModel.cadastrar(nome, email, senha, token)
             .then(
                 function (resultado) {
                     res.json(resultado);
@@ -94,9 +92,35 @@ function cadastrar(req, res) {
     }
 }
 
+function autenticarToken(req, res) {
+    console.log('oie auetenticart token')
+    var token = req.params.token;
+
+    usuarioModel.pesquisarToken(token)
+        .then(
+            function (resultado) {
+                if (resultado.length > 0) {
+                    res.status(200).json(resultado);
+                    cadastrar();
+                    console.log(resultado + "AQUIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII");
+                } else {
+                    res.status(204).send("Nenhum resultado encontrado!");
+                }
+            }
+        ).catch(
+            function (erro) {
+                console.log(erro);
+                console.log("Houve um erro ao buscar os avisos: ", erro.sqlMessage);
+                res.status(500).json(erro.sqlMessage);
+            }
+        );
+}
+
+
 module.exports = {
     entrar,
     cadastrar,
     listar,
+    autenticarToken,
     testar
 }
--- site/src/routes/usuarios.js
+++ site/src/routes/usuarios.js
@@ -20,4 +20,8 @@ router.post("/autenticar", function (req, res) {
     usuarioController.entrar(req, res);
 });
 
+router.get("/autenticarT", function (req, res) {
+    usuarioController.autenticarToken(req, res);
+})
+
 module.exports = router;
