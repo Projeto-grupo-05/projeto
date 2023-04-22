bf1f1ea15ec14a07c651f92a20f5482c57779862
DiegoCmoreira <diego.moreira@sptech.school>
2023-04-22 14:48:57 -0300

    CSS/ Dashboard completa com icons e chartjs

--- site/public/js/dashboard.js
+++ site/public/js/dashboard.js
@@ -3,25 +3,43 @@ const ctx = document.getElementById('dashboard');
 new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['semana 1', 'semana 2', 'semana 3', 'semana 4'],
+        labels: ['Semana 1', 'Semana 2', 'Semana 3', 'Semana 4'],
         datasets: [{
-            label: 'Progresso',
-            data: [10, 20, 21, 17, 15],
-            borderWidth: 1
-        }, {
             label: 'Tarefas',
             data: [12, 12, 14, 5, 2],
-            borderWidth: 1,
+            backgroundColor: [`rgb(222, 42, 37)`],
+            borderWidth: 3
+        }, {
+            label: 'Progresso',
+            data: [10, 20, 21, 17, 15],
+            backgroundColor:[ `rgb(54, 128, 194)`],
+            borderWidth: 3
         }, {
             label: 'Concluido',
             data: [8, 8, 13, 10, 13],
-            borderWidth: 1
+            backgroundColor:[`rgb(29, 142, 74)`],
+            borderWidth: 3
         }]
     },
     options: {
         scales: {
             y: {
-                beginAtZero: true
+                beginAtZero: true,
+                borderWidth: 30
+            },
+            x: {
+                grid: {
+                  offset: true
+                }
+            }
+        },
+        plugins: {
+            legend: {
+                labels: {
+                    font: {
+                        size: 16
+                    }
+                }
             }
         }
     }
