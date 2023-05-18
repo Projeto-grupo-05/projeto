943d9f751ce1bbfdcc86d4e5eff6b3abc0f2cd33
NatanFigueira <natan.figueira@sptech.school>
2023-05-17 22:17:12 -0300

    Cadastro NU default funfando

--- site/src/controllers/medidaController.js
+++ site/src/controllers/medidaController.js
@@ -41,8 +41,51 @@ function buscarMedidasEmTempoReal(req, res) {
     });
 }
 
+function checa(req, res) {
+
+    medidaModel.checa().then(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);
+        } else {
+            res.status(204).send("Nenhum resultado encontrado!")
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar as ultimas medidas.", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+}
+
+function cadastrarNU(req, res) {
+    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+    var idEmpresa = req.body.idEmpresaServer;
+
+    // Faça as validações dos valores
+
+
+        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+        medidaModel.cadastrarNU(idEmpresa)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                    console.log("MANO TA INDO");
+                }
+            ).catch(
+                function (erro) {
+                    console.log("MANO TA INDO NAO VEI, AJUDA!!");
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+
 module.exports = {
     buscarUltimasMedidas,
-    buscarMedidasEmTempoReal
-
+    buscarMedidasEmTempoReal,
+    checa,
+    cadastrarNU
 }
--- site/src/routes/medidas.js
+++ site/src/routes/medidas.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 var medidaController = require("../controllers/medidaController");
 
-router.get("/ultimas/:idAquario", function (req, res) {
-    medidaController.buscarUltimasMedidas(req, res);
+router.get("/checa", function (req, res) {
+    medidaController.checa(req, res);
 });
 
-router.get("/tempo-real/:idAquario", function (req, res) {
-    medidaController.buscarMedidasEmTempoReal(req, res);
-})
+router.post("/cadastrarNU", function (req, res) {
+    medidaController.cadastrarNU(req, res);
+});
 
 module.exports = router;
